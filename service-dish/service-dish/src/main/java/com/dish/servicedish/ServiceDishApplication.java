cd402a75ca4823f9a196816949d43ade81bfba25
Luis Florez <braian1k3@gmail.com>
2023-07-25 18:17:38 -0500

    Connect clients with Eureka server.

--- service-dish/service-dish/src/main/java/com/dish/servicedish/ServiceDishApplication.java
+++ service-dish/service-dish/src/main/java/com/dish/servicedish/ServiceDishApplication.java
@@ -2,8 +2,10 @@ package com.dish.servicedish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ServiceDishApplication {
 
 	public static void main(String[] args) {
