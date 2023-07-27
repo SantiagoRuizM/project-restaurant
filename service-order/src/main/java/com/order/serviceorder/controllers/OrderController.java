75e87492cffd71a8cfa0dc466942b8c126150b0e
Luis Florez <braian1k3@gmail.com>
2023-07-27 08:01:20 -0500

    Star process of pagination.

--- service-order/src/main/java/com/order/serviceorder/controllers/OrderController.java
+++ service-order/src/main/java/com/order/serviceorder/controllers/OrderController.java
@@ -4,6 +4,7 @@ import com.order.serviceorder.dtos.order.OrderRequestDto;
 import com.order.serviceorder.dtos.order.OrderResponseDto;
 import com.order.serviceorder.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,11 @@ public class OrderController {
         Map<String, String> map = new HashMap<>();
         map.put("message", "Created success!");
         return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getAll/{state}/{campus}")
+    public ResponseEntity<Page<OrderResponseDto>> getAllOrders(@PathVariable String state, @PathVariable Long campus, @RequestParam int page) {
+        return new ResponseEntity<>(service.getAllOrdersStateCampus(state, campus, page), HttpStatus.OK);
     }
 
     @GetMapping("/getAll")
